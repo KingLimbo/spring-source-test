6f3dfacb610f01b7bec513fdd1e9de04f6651e94
King.LF <765324072@qq.com>
2018-03-29 13:19:26 +0800

    添加备注信息

--- SpringSourceTest/src/com/tartarust/aop/Test.java
+++ SpringSourceTest/src/com/tartarust/aop/Test.java
@@ -1,12 +1,24 @@
 package com.tartarust.aop;
 
+/**
+ * 测试类
+ * @author LF
+ *
+ */
 public class Test {
 
+	/**
+	 * 主方法
+	 * 
+	 * @param args
+	 * @throws ClassNotFoundException
+	 */
 	public static void main(String[] args) throws ClassNotFoundException{   
+		// 绑定该类实现的所有接口
         IAbstractDefined abstractDefined = (IAbstractDefined)new DynaProxyAbstractDefined()
         .bind(new AbstractDefined(),new LoggerOperation());   
         abstractDefined.save("Double J");   
-        abstractDefined.save("Float J");   
+//        abstractDefined.save("Float J");   
           
     }   
 }
