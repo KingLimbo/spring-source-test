e9a13910ccb2942b5457cf54584fd7e6e5c98b98
King.LF <765324072@qq.com>
2018-03-29 13:22:23 +0800

    备注

--- SpringSourceTest/src/com/tartarust/aop/Test.java
+++ SpringSourceTest/src/com/tartarust/aop/Test.java
@@ -16,7 +16,8 @@ public class Test {
 	public static void main(String[] args) throws ClassNotFoundException{   
 		// 绑定该类实现的所有接口
         IAbstractDefined abstractDefined = (IAbstractDefined)new DynaProxyAbstractDefined()
-        .bind(new AbstractDefined(),new LoggerOperation());   
+        	.bind(new AbstractDefined(),new LoggerOperation()); 
+        // save方法
         abstractDefined.save("Double J");   
 //        abstractDefined.save("Float J");   
           
